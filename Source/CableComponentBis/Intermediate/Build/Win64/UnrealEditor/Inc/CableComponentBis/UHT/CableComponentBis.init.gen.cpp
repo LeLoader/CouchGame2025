9fecbd3abce7d2497b99acefcddcb74e47bacaa4
LeLoader <leloaderpro@gmail.com>
2025-10-16 12:06:47 +0200

    Copy cable from UE

---
+++ Source/CableComponentBis/Intermediate/Build/Win64/UnrealEditor/Inc/CableComponentBis/UHT/CableComponentBis.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeCableComponentBis_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_CableComponentBis;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_CableComponentBis()
+	{
+		if (!Z_Registration_Info_UPackage__Script_CableComponentBis.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/CableComponentBis",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x15907E0A,
+				0x981C5D58,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_CableComponentBis.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_CableComponentBis.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_CableComponentBis(Z_Construct_UPackage__Script_CableComponentBis, TEXT("/Script/CableComponentBis"), Z_Registration_Info_UPackage__Script_CableComponentBis, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x15907E0A, 0x981C5D58));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
