319bd1611ece2f61aee7556899d035125375c24e
LeLoader <leloaderpro@gmail.com>
2025-10-22 17:04:19 +0200

    Gamepad input

--- Source/CouchGame2025/Runtime/Private/Global/CouchGame2025GameMode.cpp
+++ Source/CouchGame2025/Runtime/Private/Global/CouchGame2025GameMode.cpp
@@ -17,5 +17,6 @@ ACouchGame2025GameMode::ACouchGame2025GameMode()
 
 void ACouchGame2025GameMode::StartPlay()
 {
-	// GetGameInstance()->GetSubsystem<ULocalMultiplayerSubsystem>()->CreateAndInitPlayers(ELocalMultiplayerInputMappingType::InGame);
+	Super::StartPlay();
+	GetGameInstance()->GetSubsystem<ULocalMultiplayerSubsystem>()->CreateAndInitPlayers(ELocalMultiplayerInputMappingType::InGame);
 }
