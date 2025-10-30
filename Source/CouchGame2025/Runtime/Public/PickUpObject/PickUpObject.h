8d756925dcc93a861d8e1dbdeee5cf022ae08e74
Theoferrarini <theoferrarini6@gmail.com>
2025-10-30 18:40:21 +0100

    [add] objectgravity component

--- Source/CouchGame2025/Runtime/Public/PickUpObject/PickUpObject.h
+++ Source/CouchGame2025/Runtime/Public/PickUpObject/PickUpObject.h
@@ -5,7 +5,6 @@
 #include "CoreMinimal.h"
 #include "CouchGame2025/Runtime/Public/Interface/Interactable.h"
 #include "GameFramework/Actor.h"
-#include "CouchGame2025/Runtime/Public/Component/PlanetaryMovementComponent.h"
 #include "PickUpObject.generated.h"
 
 UCLASS()
@@ -27,11 +26,6 @@ protected:
 	UPROPERTY(EditAnywhere)
 	UStaticMeshComponent* Mesh;
 
-	UPROPERTY(EditAnywhere, Category = "Gravity")
-	FVector PlanetCenter = FVector::ZeroVector;
-
-	UPROPERTY(EditAnywhere, Category = "Gravity")
-	float GravityStrength = 980.0f;
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
