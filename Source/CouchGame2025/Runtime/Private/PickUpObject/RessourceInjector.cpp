66cb8ed813c6d341f165728a48cdb57b91c1b832
Theoferrarini <theoferrarini6@gmail.com>
2025-11-16 22:11:22 +0100

    [add] start injector (not tested yet)

---
+++ Source/CouchGame2025/Runtime/Private/PickUpObject/RessourceInjector.cpp
@@ -0,0 +1,101 @@
+﻿#include "CouchGame2025/Runtime/Public/PickUpObject/RessourceInjector.h"
+#include "Component/RessourceContainerComponent.h"
+
+ARessourceInjector::ARessourceInjector()
+{
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+void ARessourceInjector::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void ARessourceInjector::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+
+    if (CurrentSource == nullptr)
+        return;
+
+    if (IsFull() || CurrentSource->IsContainerEmpty())
+    {
+        StopTransferFromContainer();
+        return;
+    }
+
+    float Wanted = TransferRate * DeltaSeconds;
+    float Removed = CurrentSource->RemoveRessource(Wanted);
+    if (Removed <= 0.f)
+    {
+        StopTransferFromContainer();
+        return;
+    }
+
+    float Added = AddRessource(Removed);
+    if (Added < Removed)
+    {
+        float ReturnAmount = Removed - Added;
+        CurrentSource->AddRessource(ReturnAmount);
+        StopTransferFromContainer();
+    }
+
+    if (CurrentSource->IsContainerEmpty())
+    {
+        StopTransferFromContainer();
+    }
+}
+
+float ARessourceInjector::AddRessource(float Amount)
+{
+    if (Amount <= 0.f) return 0.f;
+    float Space = FMath::Max(0.f, MaxRessourceAmount - CurrentRessourceAmount);
+    float ToAdd = FMath::Min(Amount, Space);
+    CurrentRessourceAmount += ToAdd;
+    return ToAdd;
+}
+
+float ARessourceInjector::RemoveRessource(float Amount)
+{
+    if (Amount <= 0.f) return 0.f;
+    float ToRemove = FMath::Min(Amount, CurrentRessourceAmount);
+    CurrentRessourceAmount -= ToRemove;
+    return ToRemove;
+}
+
+float ARessourceInjector::TransferFromContainerInstant(URessourceContainerComponent* Source, float Amount)
+{
+    if (Source == nullptr || Amount <= 0.f) return 0.f;
+
+    float Removed = Source->RemoveRessource(Amount);
+    if (Removed <= 0.f) return 0.f;
+
+    float Added = AddRessource(Removed);
+    if (Added < Removed)
+    {
+        float ReturnAmount = Removed - Added;
+        Source->AddRessource(ReturnAmount);
+    }
+    return Added;
+}
+
+void ARessourceInjector::StartTransferFromContainer(URessourceContainerComponent* Source)
+{
+    if (Source == nullptr) return;
+    CurrentSource = Source;
+}
+
+void ARessourceInjector::StopTransferFromContainer()
+{
+    CurrentSource = nullptr;
+}
+
+bool ARessourceInjector::IsFull() const
+{
+    return FMath::Abs(CurrentRessourceAmount - MaxRessourceAmount) < UE_KINDA_SMALL_NUMBER;
+}
+
+bool ARessourceInjector::IsEmpty() const
+{
+    return CurrentRessourceAmount < UE_KINDA_SMALL_NUMBER;
+}
---
+++ Source/CouchGame2025/Runtime/Public/PickUpObject/RessourceInjector.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Component/RessourceContainerComponent.h"
+#include "RessourceInjector.generated.h"
+
+UCLASS(Blueprintable)
+class COUCHGAME2025_API ARessourceInjector : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ARessourceInjector();
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void Tick(float DeltaSeconds) override;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ressource")
+    FRessourceType RessourceType;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ressource")
+    float MaxRessourceAmount = 1.f;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Ressource")
+    float CurrentRessourceAmount = 0.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Transfer")
+    float TransferRate = 1.f;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Transfer")
+    URessourceContainerComponent* CurrentSource = nullptr;
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "Ressource")
+    float AddRessource(float Amount);
+
+    UFUNCTION(BlueprintCallable, Category = "Ressource")
+    float RemoveRessource(float Amount);
+
+    UFUNCTION(BlueprintCallable, Category = "Transfer")
+    float TransferFromContainerInstant(URessourceContainerComponent* Source, float Amount);
+
+    UFUNCTION(BlueprintCallable, Category = "Transfer")
+    void StartTransferFromContainer(URessourceContainerComponent* Source);
+
+    UFUNCTION(BlueprintCallable, Category = "Transfer")
+    void StopTransferFromContainer();
+
+    UFUNCTION(BlueprintGetter, Category = "Ressource")
+    bool IsFull() const;
+
+    UFUNCTION(BlueprintGetter, Category = "Ressource")
+    bool IsEmpty() const;
+};
