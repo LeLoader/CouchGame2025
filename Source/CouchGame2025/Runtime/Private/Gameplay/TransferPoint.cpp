d4c932f7868b161ee38a86bf298b4e2fc3acb668
Theoferrarini <theoferrarini6@gmail.com>
2025-11-19 11:10:06 +0100

    [add] transferpoint for the transition of intern and extern planet

---
+++ Source/CouchGame2025/Runtime/Private/Gameplay/TransferPoint.cpp
@@ -0,0 +1,18 @@
+﻿#include "CouchGame2025/Runtime/Public/Gameplay/TransferPoint.h"
+
+ATransferPoint::ATransferPoint()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ATransferPoint::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ATransferPoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
