04d4d9e8b3e17b6732382d43af784d9fe7f6ed4a
Theoferrarini <theoferrarini6@gmail.com>
2025-11-19 14:49:14 +0100

    [modify] transfer point can see the sphere collision of the internal planet

--- Source/CouchGame2025/Runtime/Public/Gameplay/TransferPoint.h
+++ Source/CouchGame2025/Runtime/Public/Gameplay/TransferPoint.h
@@ -1,23 +1,26 @@
-﻿#pragma once
-
-#include "CoreMinimal.h"
+﻿#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
 #include "GameFramework/Actor.h"
 #include "TransferPoint.generated.h"
 
 UCLASS()
 class COUCHGAME2025_API ATransferPoint : public AActor
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	ATransferPoint();
+    ATransferPoint();
 
-	UPROPERTY(EditAnywhere)
-	AActor *Bp_Planet;
-	
+    UPROPERTY(EditAnywhere)
+    AActor *Bp_Planet;
+
 protected:
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
 
 public:
-	virtual void Tick(float DeltaTime) override;
+    virtual void Tick(float DeltaTime) override;
+
+private:
+    UPROPERTY(VisibleAnywhere)
+    USphereComponent* InternalPlanet = nullptr;
 };
